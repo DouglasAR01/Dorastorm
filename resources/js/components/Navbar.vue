80126a00f8f3e669afd0f257c85ca3c9f25281a4
Douglas Ramírez <33143337+DouglasAR01@users.noreply.github.com>
2021-02-26 00:46:57 -0500

    Log out button bugfix
    Users could click multiple times log out button

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -6,7 +6,7 @@
     <ul class="navbar-nav">
       <li class="nav-item" v-if="!isLoggedIn">
         <router-link :to="{ name: 'login' }" class="nav-link">
-          Log-in
+          Log in
         </router-link>
       </li>
       <li class="nav-item dropdown" v-if="isLoggedIn">
@@ -40,7 +40,7 @@
         </div>
       </li>
       <li class="nav-item" v-if="isLoggedIn">
-        <a href="#" class="nav-link" @click.prevent="logout">Log-out</a>
+        <a href="#" class="nav-link" @click.prevent="logout" :class="[{'disabled':isLoggingOut}]">Log out</a>
       </li>
     </ul>
   </nav>
@@ -50,6 +50,11 @@ import { mapState } from "vuex";
 import Permissions from "../services/role-permissions";
 // Log-in and Log-out buttons logic idea taken from https://www.youtube.com/watch?v=8Uwn5M6WTe0
 export default {
+  data () {
+    return {
+      isLoggingOut: false
+    };
+  },
   computed: {
     ...mapState({
       isLoggedIn: (state) => state.isLoggedIn,
@@ -61,12 +66,14 @@ export default {
   },
   methods: {
     async logout() {
+      this.isLoggingOut = true;
       try {
         await axios.post("/api/logout");
       } catch (error) {
         // No internet conection
       }
       this.$store.dispatch("logout");
+      this.isLoggingOut = false;
       this.$router.push({ name: "login" });
     },
   },
