140e892a289ea9549bacdff314b36cdc20093589
Douglas Ramírez <33143337+DouglasAR01@users.noreply.github.com>
2021-03-11 11:35:39 -0500

    Added a Footer and Fab icons

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="jumbotron jumbotron-fluid mt-auto">
+    <div class="row justify-content-between">
+      <div class="col-auto"></div>
+      <div class="col-auto">
+        <a href="https://twitter.com/nuwebsco"
+          ><i class="fab fa-twitter"></i
+        ></a>
+      </div>
+      <div class="col-auto">
+        <a href="https://www.facebook.com/nuwebsco"
+          ><i class="fab fa-facebook-f"></i
+        ></a>
+      </div>
+      <div class="col-auto">
+        <a href="https://www.instagram.com/nuwebsco/"
+          ><i class="fab fa-instagram"></i
+        ></a>
+      </div>
+      <div class="col-auto"></div>
+    </div>
+    <div class="row justify-content-center">Lang</div>
+    <div class="row justify-content-center">
+      <p>©Copyright {{ year }}</p>
+    </div>
+  </div>
+</template>
+<script>
+import dayjs from "dayjs";
+export default {
+  computed: {
+    year() {
+      return dayjs().year();
+    },
+  },
+};
+</script>
