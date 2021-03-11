9372d6bf03293fb68d69ee9eeb982562aea152ae
Douglas Ramírez <33143337+DouglasAR01@users.noreply.github.com>
2021-03-11 11:36:06 -0500

    Index ViewHeight set to 100% and footer added

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>
+  <div class="d-flex flex-column min-vh-100">
     <navbar></navbar>
     <div class="container mt-4 mb-4 pr-4 pl-4">
       <router-view></router-view>
     </div>
+    <footer-component></footer-component>
   </div>
 </template>
 <script>
 import Navbar from "./components/Navbar";
+import FooterComponent from "./components/Footer";
 export default {
-  components: { Navbar },
-  
-}
+  components: { Navbar, FooterComponent },
+};
 </script>
