13dbc9e70953d0fc4a9dee1e0eccc2095cc0b049
Douglas Ramírez <33143337+DouglasAR01@users.noreply.github.com>
2021-03-22 17:30:30 -0500

    Create RolePermissionSelector.vue

---
+++ resources/js/components/roles/RolePermissionSelector.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h3>{{ $t("modules.roles.select_permissions") }}</h3>
+    <div
+      class="border-bottom pb-2 mt-2"
+      v-for="(content, categorie) in permissionsCategories"
+      :key="categorie"
+    >
+      <h6>{{ categorie }}</h6>
+      <div class="custom-control custom-checkbox" v-for="permission in content" :key="permission" @change="selected">
+        <input type="checkbox" :id="permission" :value="permission" class="custom-control-input" v-model="selected_permissions">
+        <label :for="permission" class="custom-control-label">{{permission}}</label>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Permissions from "../../services/role-permissions";
+export default {
+  data() {
+    return {
+      loading: false,
+      selected_permissions: [],
+    };
+  },
+  computed: {
+    permissionsCategories() {
+      return Permissions.categorizePermissions(
+        this.$store.getters.getUserPermissions
+      );
+    },
+  },
+  methods: {
+    selected() {
+      this.$emit("selected", this.selected_permissions);
+    },
+  },
+};
+</script>
