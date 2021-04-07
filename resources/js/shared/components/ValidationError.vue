f253ff248d05bcb1cc3d22c0780b1051639c15a6
Douglas Ramírez <33143337+DouglasAR01@users.noreply.github.com>
2021-04-06 19:12:58 -0500

    ValidationError component refactored

--- resources/js/shared/components/ValidationError.vue
+++ resources/js/shared/components/ValidationError.vue
@@ -1,19 +1,38 @@
 <template>
-  <div v-if="errors != null">
-    <div class="invalid-feedback" v-for="error in errors" :key="'err'+error">
-      {{ error }}
+  <div>
+    <slot :e="hasErrors" />
+    <div v-if="hasErrors">
+      <div
+        class="invalid-feedback"
+        v-for="error in errorFor"
+        :key="'err' + error"
+      >
+        {{ error }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["errors"],
+  props: ["errors", "name"],
+  computed: {
+    hasErrors() {
+      return (
+        this.errors &&
+        this.errors[this.name] &&
+        this.errors[this.name].length > 0
+      );
+    },
+    errorFor() {
+      return this.errors[this.name];
+    },
+  },
 };
 </script>
 
 <style scoped>
-.form-control.is-invalid ~ div > .invalid-feedback {
+.is-invalid ~ div > .invalid-feedback {
   display: block;
 }
 </style>
