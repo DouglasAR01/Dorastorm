f6409dc5008386387bfa62986ab62030339521fc
Douglas Ramírez <33143337+DouglasAR01@users.noreply.github.com>
2021-07-26 15:29:03 -0500

    (MINOR) Carousel components

---
+++ resources/js/shared/components/carousel/BaseCarousel.vue
@@ -0,0 +1,73 @@
+<template>
+  <div
+    class="carousel slide carousel-fade"
+    :id="cid"
+    data-ride="carousel"
+    data-interval="6000"
+  >
+    <ol class="carousel-indicators">
+      <li
+        v-for="(item, index) in content"
+        :key="index"
+        :data-target="`#${cid}`"
+        :data-slide-to="index"
+        :class="[{ active: index === 0 }]"
+      ></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+      <carousel-item
+        v-for="(item, index) in content"
+        :key="'i' + index"
+        :class="[{ active: index === 0 }]"
+        :item="item"
+      />
+    </div>
+    <a
+      class="carousel-control-prev"
+      :href="`#${cid}`"
+      role="button"
+      data-slide="prev"
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a
+      class="carousel-control-next"
+      :href="`#${cid}`"
+      role="button"
+      data-slide="next"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</template>
+<script>
+import CarouselItem from "./CarouselItem";
+export default {
+  components: {
+    CarouselItem,
+  },
+  props: {
+    content: {
+      type: Array,
+      required: true,
+      validator(payload) {
+        // There must be at least 2 elements for the carousel
+        if (payload.length < 2) {
+          return false;
+        }
+        return true;
+      },
+    },
+  },
+  data() {
+    return {
+      cid: null,
+    };
+  },
+  mounted() {
+    this.cid = "carousel"+ this._uid;
+  },
+};
+</script>
