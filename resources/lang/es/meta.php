1cf65e771cf0619d63cc4211c431097639751509
Douglas Ramírez <33143337+DouglasAR01@users.noreply.github.com>
2021-09-30 15:44:23 -0500

    (MINOR) Noscript tag support

--- resources/lang/es/meta.php
+++ resources/lang/es/meta.php
@@ -2,4 +2,8 @@
 return [
     'title' => 'Dorastorm',
     'description' => 'Dorastorm, desarrollado por Nuwebs en Colombia.',
+
+    'no_script' => [
+        'js_warning' => 'Este sitio require Javascript para funcionar.',
+    ],
 ];
