c6de8ff84223aaf8f5cc2f1e7d38c88541e271a8
Douglas Ramírez <33143337+DouglasAR01@users.noreply.github.com>
2021-11-04 18:09:40 -0500

    Navbar bugfix + Bing SEO improvement

--- resources/js/components/TheNavbar.vue
+++ resources/js/components/TheNavbar.vue
@@ -1,25 +1,47 @@
 <template>
   <nav class="navbar navbar-expand-sm navbar-light bg-light border-bottom">
-    <router-link :to="{ name: 'home', params: {locale: $route.params.locale} }" class="navbar-brand">
+    <router-link
+      :to="{ name: 'home', params: { locale: $route.params.locale } }"
+      class="navbar-brand"
+    >
       Dorastorm
     </router-link>
     <navbar-collapse-button />
     <base-navbar-collapse-list>
       <!-- Post index -->
       <single-navbar-list-element v-if="!isLoggedIn">
-        <router-link :to="{ name: 'posts-index', params: {locale: $route.params.locale} }" class="nav-link">
+        <router-link
+          :to="{
+            name: 'posts-index',
+            params: { locale: $route.params.locale },
+          }"
+          class="nav-link"
+        >
           {{ $t("modules.posts.index") }}
         </router-link>
       </single-navbar-list-element>
       <!-- Login -->
       <single-navbar-list-element v-if="!isLoggedIn">
-        <router-link :to="{ name: 'login', params: {locale: $route.params.locale} }" class="nav-link">
+        <router-link
+          :to="{ name: 'login', params: { locale: $route.params.locale } }"
+          class="nav-link"
+        >
           {{ $t("navbar.login") }}
         </router-link>
       </single-navbar-list-element>
       <!-- Quotes -->
-      <single-navbar-list-element v-if="isLoggedIn && checkUserPermission(loggedUser, corePms.READ_QUOTES)">
-        <router-link :to="{ name: 'quotes-index', params: {locale: $route.params.locale} }" class="nav-link">
+      <single-navbar-list-element
+        v-if="
+          isLoggedIn && checkUserPermission(loggedUser, corePms.READ_QUOTES)
+        "
+      >
+        <router-link
+          :to="{
+            name: 'quotes-index',
+            params: { locale: $route.params.locale },
+          }"
+          class="nav-link"
+        >
           {{ $t("modules.quotes.index") }}
         </router-link>
       </single-navbar-list-element>
@@ -29,24 +51,39 @@
           {{ $t("modules.posts.posts") }}
         </template>
         <router-link
-          :to="{ name: 'posts-create', params: {locale: $route.params.locale} }"
+          :to="{
+            name: 'posts-create',
+            params: { locale: $route.params.locale },
+          }"
           class="nav-link"
           v-if="checkUserPermission(loggedUser, corePms.CREATE_POSTS)"
         >
           {{ $t("modules.posts.create") }}
         </router-link>
         <router-link
-          :to="{ name: 'user-posts', params: {locale: $route.params.locale} }"
+          :to="{ name: 'user-posts', params: { locale: $route.params.locale } }"
           class="nav-link"
           v-if="checkUserPermission(loggedUser, corePms.CREATE_POSTS)"
         >
           {{ $t("modules.posts.my_posts") }}
         </router-link>
         <hr class="w-75 my-1" />
-        <router-link class="nav-link" :to="{ name: 'posts-index', params: {locale: $route.params.locale} }">
+        <router-link
+          class="nav-link"
+          :to="{
+            name: 'posts-index',
+            params: { locale: $route.params.locale },
+          }"
+        >
           {{ $t("modules.posts.index") }}
         </router-link>
-        <router-link class="nav-link" :to="{ name: 'private-posts-index', params: {locale: $route.params.locale} }">
+        <router-link
+          class="nav-link"
+          :to="{
+            name: 'private-posts-index',
+            params: { locale: $route.params.locale },
+          }"
+        >
           {{ $t("modules.posts.private_index") }}
         </router-link>
       </navbar-list-dropdown-element>
@@ -66,14 +103,20 @@
           {{ $t("navbar.users.users") }}
         </template>
         <router-link
-          :to="{ name: 'users-index', params: {locale: $route.params.locale} }"
+          :to="{
+            name: 'users-index',
+            params: { locale: $route.params.locale },
+          }"
           v-if="checkUserPermission(loggedUser, corePms.READ_USERS)"
           class="nav-link"
         >
           {{ $t("navbar.users.read") }}
         </router-link>
         <router-link
-          :to="{ name: 'users-create', params: {locale: $route.params.locale} }"
+          :to="{
+            name: 'users-create',
+            params: { locale: $route.params.locale },
+          }"
           v-if="checkUserPermission(loggedUser, corePms.CREATE_USERS)"
           class="nav-link"
         >
@@ -96,7 +139,10 @@
           {{ $t("navbar.roles.title") }}
         </template>
         <router-link
-          :to="{ name: 'roles-index', params: {locale: $route.params.locale} }"
+          :to="{
+            name: 'roles-index',
+            params: { locale: $route.params.locale },
+          }"
           v-if="checkUserPermission(loggedUser, corePms.READ_ROLES)"
           class="nav-link"
         >
@@ -104,13 +150,19 @@
         </router-link>
         <router-link
           class="nav-link"
-          :to="{ name: 'roles-create', params: {locale: $route.params.locale} }"
+          :to="{
+            name: 'roles-create',
+            params: { locale: $route.params.locale },
+          }"
           v-if="checkUserPermission(loggedUser, corePms.CREATE_ROLES)"
           >{{ $t("navbar.roles.create") }}</router-link
         >
       </navbar-list-dropdown-element>
       <single-navbar-list-element classes="ml-sm-3" v-if="isLoggedIn">
-        <router-link :to="{ name: 'me', params: {locale: $route.params.locale} }" class="nav-link">
+        <router-link
+          :to="{ name: 'me', params: { locale: $route.params.locale } }"
+          class="nav-link"
+        >
           <span> <i class="fas fa-user"></i> {{ userName }} </span>
         </router-link>
       </single-navbar-list-element>
@@ -148,7 +200,10 @@ export default {
   },
   computed: {
     userName() {
-      return this.loggedUser.name.split(" ")[0];
+      if (this.isLoggedIn) {
+        return this.loggedUser.name.split(" ")[0];
+      }
+      return null;
     },
   },
   methods: {
@@ -161,7 +216,10 @@ export default {
       }
       this.$store.dispatch("logout");
       this.isLoggingOut = false;
-      this.$router.push({ name: "login", params: {locale: this.$route.params.locale} });
+      this.$router.push({
+        name: "login",
+        params: { locale: this.$route.params.locale },
+      });
     },
   },
 };
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,8 +13,8 @@
 
     {{-- Your facebook domain verification --}}
     <meta name="facebook-domain-verification" content="">
-    
-    <meta property="og:url" content="{{config('app.url')}}">
+
+    <meta property="og:url" content="{{ config('app.url') }}">
     <meta property="og:title" content="@lang('meta.title')">
     <meta property="og:description" content="@lang('meta.description')">
     <meta property="og:type" content="website">
@@ -26,11 +26,9 @@
     <meta name="twitter:site" content="@nuwebsco">
 
     @foreach ($locales as $locale)
-    <link rel="alternate" hreflang="{{$locale}}"
-        href="{{config('app.url')}}/{{$locale}}" />
+        <link rel="alternate" hreflang="{{ $locale }}" href="{{ config('app.url') }}/{{ $locale }}" />
     @endforeach
-    <link rel="alternate" hreflang="x-default"
-       href="{{config('app.url')}}" />
+    <link rel="alternate" hreflang="x-default" href="{{ config('app.url') }}" />
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src=" {{ asset('js/app.js') }}" defer></script>
@@ -39,13 +37,15 @@
 
 <body>
     <div id="app">
+        <h1 class="d-none">@lang('meta.title')</h1>
         <the-index></the-index>
         <noscript>
             <div style="height: 100vh">
                 <div class="container h-100">
                     <div class="row h-100 align-items-center">
                         <div class="col-sm-2 offset-sm-2 d-flex justify-content-end">
-                            <img src="{{asset('images/DS-Logo.png')}}" alt="Logo" class="img-fluid" style="max-height: 25vh">
+                            <img src="{{ asset('images/DS-Logo.png') }}" alt="Logo" class="img-fluid"
+                                style="max-height: 25vh">
                         </div>
                         <div class="col-sm-6">
                             <h1 class="display-3">@lang('meta.title')</h1>
